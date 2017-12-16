c2e6158c638b296eed48bf20909ec26fb63f5556
Christophe Coquelet <coquelet.c@gmail.com>
2017-12-16 23:38:50 +0100

    add possibility to pass --folder= to save all pictures

--- init.rb
+++ init.rb
@@ -4,6 +4,18 @@ require 'open-uri'
 require 'fileutils'
 require 'ruby-progressbar'
 
+
+args = {}
+
+ARGV.each do |arg|
+  match = /--(?<key>.*?)=(?<value>.*)/.match(arg)
+  args[match[:key]] = match[:value]
+end
+
+destination = './Wallpapers'
+
+if args.has_key? 'folder' then destination = args['folder'] end
+
 maller_page = "http://justinmaller.com"
 global_wallpaper = "wallpapers/"
 first_page_css_selector = 'div.project-container > div:nth-child(1) > div:nth-child(3) > a:nth-child(1)'
@@ -12,7 +24,7 @@ second_page_css_selector = '#wallwindow > img:nth-child(1)'
 page = Nokogiri::HTML(open("#{maller_page}/#{global_wallpaper}"))
 downloadsArray = page.css(first_page_css_selector)
 
-FileUtils.mkdir_p './Wallpapers'
+FileUtils.mkdir_p destination
 
 prog = ProgressBar.create(:title => "Downloading Maller Wallpapers", :total => downloadsArray.length, :progress_mark => '#', :remainder_mark => '-')
 
@@ -22,7 +34,7 @@ downloadsArray.each do |node|
   currentImg = currentPage.css(second_page_css_selector).attr('src')
   currentTitle = currentPage.css('#title').text.gsub(/\s+/, '_')
   prog.title = "Downloading #{currentTitle}.jpg"
-  open("./Wallpapers/#{currentTitle}.jpg", 'wb') do |file|
+  open("#{destination}/#{currentTitle}.jpg", 'wb') do |file|
     file << open(currentImg).read
     prog.increment
   end
